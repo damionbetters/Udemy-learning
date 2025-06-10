53ff0d97fd9fc55d59582621cb70759328ecf660
Damion Betters <153688977+damionbetters@users.noreply.github.com>
2025-06-10 14:24:09 -0500

    Learning Backend

--- Full Stack/24.0 Backend/24.2 Native Modules/index.js
+++ Full Stack/24.0 Backend/24.2 Native Modules/index.js
@@ -1,18 +1,35 @@
 "use strict";
 //
-import { readFile, writeFile } from "node:fs";
+// import { readFile, writeFile } from "node:fs";
+import express from "express";
+import { fileURLToPath } from "url";
+import { dirname } from "path";
 
 //
-let data = "What up Doe?!";
+// let data = "What up Doe?!";
+const app = express();
+const PORT = 3000;
+const fileName = fileURLToPath(import.meta.url);
+const directoryName = dirname(fileName);
+
+// //
+// writeFile("message.txt", data, (err) => {
+//   if (err) throw err;
+//   console.log("The file has been saved");
+// });
+
+// //
+// readFile("message.txt", "utf-8", (err, data) => {
+//   if (err) throw err;
+//   console.log(data);
+// });
 
 //
-writeFile("message.txt", data, (err) => {
-  if (err) throw err;
-  console.log("The file has been saved");
+
+app.get("/", (req, res) => {
+  res.sendFile(directoryName + "/public/index.html");
 });
 
-//
-readFile("message.txt", "utf-8", (err, data) => {
-  if (err) throw err;
-  console.log(data);
+app.listen(PORT, () => {
+  console.log(`Server is listening on port ${PORT}`);
 });
